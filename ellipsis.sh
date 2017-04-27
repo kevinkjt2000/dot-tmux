f75dc7b3a0d8dc7394c6a6d8271c97df6b6eddf1
Kevin Tindall <kevinkjt2000@gmail.com>
2017-04-27 03:13:51 -0500

    clean and update plugins on ellipsis pull

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,19 +3,21 @@
 # kevinkjt2000/tmux ellipsis package
 
 pkg.install() {
-  git submodule update --init 
+	git submodule update --init
 }
 
 pkg.link() {
-  fs.link_file tmux.conf 
-  mkdir -p ~/.tmux/plugins
-  fs.link_file tmux/plugins/tpm ~/.tmux/plugins/tpm
-  fs.link_file tmux/tmuxline-snapshot.conf ~/.tmux/tmuxline-snapshot.conf
-  ~/.tmux/plugins/tpm/bin/install_plugins
+	fs.link_file tmux.conf
+	mkdir -p ~/.tmux/plugins
+	fs.link_file tmux/plugins/tpm ~/.tmux/plugins/tpm
+	fs.link_file tmux/tmuxline-snapshot.conf ~/.tmux/tmuxline-snapshot.conf
+	~/.tmux/plugins/tpm/bin/install_plugins
 }
 
 pkg.pull() {
-  git.pull
-  git submodule update --remote
-  ~/.tmux/plugins/tpm/bin/install_plugins
+	git.pull
+	git submodule update --remote
+	~/.tmux/plugins/tpm/bin/clean_plugins
+	~/.tmux/plugins/tpm/bin/install_plugins
+	~/.tmux/plugins/tpm/bin/update_plugins all
 }
